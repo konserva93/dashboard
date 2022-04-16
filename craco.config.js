6cf1784622ccd6ccd08caebb6a8c865ae876178c
konserva93 <itsergeev93@gmail.com>
2022-04-16 15:58:35 +0400

    Добавлены алиасы для путей и линтер

---
+++ craco.config.js
@@ -0,0 +1,13 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const cracoAlias = require('craco-alias');
+
+module.exports = {
+  plugins: [{
+    plugin: cracoAlias,
+    options: {
+      source: 'tsconfig',
+      baseUrl: '.',
+      tsConfigPath: './tsconfig.path.json',
+    },
+  }],
+};
